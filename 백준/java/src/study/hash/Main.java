f3f7ec216b60e98a8902f4a4cf7761cb4da75932
bomikim0214 <rlaqhals05@naver.com>
2026-03-23 17:44:43 +0900

    basic: 버퍼를 이용한 입출력

---
+++ 백준/java/src/study/hash/Main.java
@@ -0,0 +1,14 @@
+package study.hash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var tokenizer = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+    }
+}

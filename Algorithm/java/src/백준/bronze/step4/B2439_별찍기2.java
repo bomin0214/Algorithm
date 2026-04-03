1bcaeb74d6ceb73a9be7d8b396a5c43df80cfa76
bomikim0214 <rlaqhals05@naver.com>
2026-04-03 17:09:47 +0900

    boj: 2439 별찍기2 (Bronze 4)

--- Algorithm/java/src/백준/bronze/step4/B2439_별찍기2.java
+++ Algorithm/java/src/백준/bronze/step4/B2439_별찍기2.java
@@ -6,16 +6,15 @@ public class B2439_별찍기2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N-i; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N-i; j++) {
                 bw.write(" ");
             }
 
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j <= i; j++) {
                 bw.write("*");
             }
             bw.write("\n");

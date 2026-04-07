dd4b1cb205f78e52f475c033c68dc6e5359cef52
bomikim0214 <rlaqhals05@naver.com>
2026-04-07 15:28:00 +0900

    boj: 2562 최댓값 (Bronze 3)

--- Algorithm/java/src/백준/bronze/step3/B2562_최댓값.java
+++ Algorithm/java/src/백준/bronze/step3/B2562_최댓값.java
@@ -7,9 +7,31 @@ public class B2562_최댓값 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int array;
-        while (true){
-            array = Integer.parseInt(br.readLine());
+        int array[] = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            int N = Integer.parseInt(br.readLine());
+            array[i]=N;
         }
+
+        int maximum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            if(array[i] > maximum){
+                maximum = array[i];
+            }
+        }
+
+        int num = 0;
+
+        for (int i = 0; i <9 ; i++) {
+            if(array[i]==maximum)
+                num=i+1;
+        }
+
+        bw.write(maximum+""+"\n");
+        bw.write(num+"");
+        bw.flush();
+        bw.close();
     }
 }

1066a93ede6ead6fa50ef6dcff88b985f731ffed
bomikim0214 <rlaqhals05@naver.com>
2026-04-10 15:38:55 +0900

    boj: 11659 구간합구하기4 refactoring (Silver 3)

--- Algorithm/java/src/백준/silver/step3/B11659_구간합구하기4.java
+++ Algorithm/java/src/백준/silver/step3/B11659_구간합구하기4.java
@@ -13,23 +13,19 @@ public class B11659_구간합구하기4 {
         int N = Integer.parseInt(st.nextToken()); // 5
         int M = Integer.parseInt(st.nextToken()); // 3
 
-        int array[] = new int[N];
+        int S[] = new int[N+1];
+
         st = new StringTokenizer(br.readLine()); // 5 4 3 2 1
-        for (int i = 0; i < array.length; i++) {
-            int arr = Integer.parseInt(st.nextToken());
-            array[i]=arr; // 배열에 넣고
+        for (int i = 1; i <= N; i++) {
+            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        int S[] = new int[M];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int one = Integer.parseInt(st.nextToken());
             int two = Integer.parseInt(st.nextToken());
 
-            for (int j = one-1; j < two; j++) {
-                S[i] += array[j];
-            }
-            bw.write(S[i]+"\n"+"");
+            bw.write(S[two]-S[one-1]+"\n");
         }
         bw.flush();
         bw.close();
